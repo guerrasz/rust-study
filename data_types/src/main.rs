e35c35bc43597e3d96c12e70f13d67ef5b4d9f90
LucasGuerraS <lucasgasardo@gmail.com>
2025-11-29 15:31:59 -0300

    feat: expore basic variables

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let integer: i32 = 42;
+    let float: f64 = 3.14;
+    let boolean: bool = true;
+    let character: char = 'R';
+    let string: &str = "Hello, Rust!";
+    let tuple: (i32, f64, char) = (integer, float, character);
+    let array: [i32; 5] = [1, 2, 3, 4, 5];
+    dbg!(integer, float, boolean, character, string, tuple, array);
 }
