8d95b4b92a498ee3dd1fa6cc6386b98bd8e87293
guerrasz <lucasgasardo@gmail.com>
2025-12-09 19:53:33 -0300

    feat: add Cargo.lock and implement Folder and File structures with example usage

--- discovery/13_vectors/src/main.rs
+++ discovery/13_vectors/src/main.rs
@@ -1,3 +1,38 @@
+use std::vec;
+
+struct File {
+    name: String,
+}
+
+struct Folder {
+    name: String,
+    contents: Vec<File>,
+}
+
+impl Folder {
+    fn new(name: String) -> Folder {
+        Folder {
+            name,
+            contents: vec![],
+        }
+    }
+
+    fn create_file(&mut self, name: String) {
+        self.contents.push(File { name });
+    }
+
+    fn delete_file(&mut self, index: usize) -> File {
+        self.contents.remove(index)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    examples();
+}
+
+fn examples() {
+    let foo: Vec<u8> = Vec::from([4, 8, 10, 12]);
+    println!("{:?}", foo);
+    let pizza_diameters = vec![4, 8, 10, 12];
+    println!("{:?}", pizza_diameters);
 }
