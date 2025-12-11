439b6ee64df08c7dc166b2cc317fd9bff289f363
guerrasz <lucasgasardo@gmail.com>
2025-12-11 20:57:44 -0300

    feat: implement main function with HashMap for sauces and meals

--- discovery/17_hash_maps/src/main.rs
+++ discovery/17_hash_maps/src/main.rs
@@ -1,3 +1,56 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let values = [
+        (
+            "Ketchup",
+            Vec::from(["French Fries", "Burgers", "Hot Dogs"]),
+        ),
+        ("Mayonese", Vec::from(["Sandwiches", "Burgers", "Coleslaw"])),
+    ];
+    let mut sauces_to_meals: HashMap<&str, Vec<&str>> = HashMap::from(values);
+
+    sauces_to_meals.insert("Mustard", Vec::from(["Hot dog", "Burgers", "Pretzels"]));
+
+    remove_key("Mayonese", &mut sauces_to_meals);
+
+    match sauces_to_meals.get("Mustard") {
+        Some(meals) => {
+            println!("{:?}", meals)
+        }
+        None => {
+            println!("No value found")
+        }
+    }
+
+    sauces_to_meals
+        .entry("Soy Sauce")
+        .or_insert(Vec::from(["Sushi", "Dumplings"]));
+
+    println!("{:?}", sauces_to_meals);
+
+    examples();
+}
+
+fn remove_key(key: &str, set: &mut HashMap<&str, Vec<&str>>) {
+    if let Some(meals) = set.remove(key) {
+        println!("{:?}", meals)
+    } else {
+        println!("{} key not found", key)
+    }
+}
+
+fn examples() {
+    let mut menu: HashMap<String, f64> = HashMap::new();
+
+    menu.insert(String::from("Carbonara"), 29.99);
+    menu.insert(String::from("Tuna"), 18.99);
+    menu.insert(String::from("Steak"), 15.99);
+
+    println!("{:?}", menu);
+    println!("{:?}", menu.get("Tuna"));
+
+    menu.entry(String::from("Sushi")).or_insert(5.99);
+
+    println!("{:?}", menu)
 }
