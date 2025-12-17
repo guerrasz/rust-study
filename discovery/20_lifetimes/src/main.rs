fd1719a23ef99f7ec746e50459c4ad2d742a7f92
guerrasz <lucasgasardo@gmail.com>
2025-12-17 19:48:20 -0300

    feat: add functions for selecting first two items and finding longest string

--- discovery/20_lifetimes/src/main.rs
+++ discovery/20_lifetimes/src/main.rs
@@ -1,3 +1,36 @@
+// using the ' (tick) to define a lifetime annotation
+fn select_first_two<'a>(items: &'a [String]) -> &'a [String] {
+    &items[..2]
+}
+
+fn longest<'a>(first: &'a str, second: &'a str) -> &'a str {
+    if first.len() > second.len() {
+        first
+    } else {
+        second
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // non-lexical lifetime
+    let mut data = vec![1, 2, 3];
+    let slice = &mut data[..];
+    println!("{:?}", slice.first());
+    data.push(2);
+
+    let cities = vec![
+        String::from("SP"),
+        String::from("RJ"),
+        String::from("LK"),
+        String::from("US"),
+        String::from("QA"),
+    ];
+
+    let first = select_first_two(&cities);
+    println!("{:?}", first);
+
+    let orlando = String::from("Orlando");
+    let sf = "San Franciso";
+    println!("{:?}", longest(&orlando, sf));
+    println!("{}", orlando);
 }
