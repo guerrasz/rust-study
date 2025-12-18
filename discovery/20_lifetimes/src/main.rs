acfd304e6983af48ff60559a55c54088d2a85938
guerrasz <lucasgasardo@gmail.com>
2025-12-18 15:34:01 -0300

    feat: enhance lifetime examples with additional functions and structs

--- discovery/20_lifetimes/src/main.rs
+++ discovery/20_lifetimes/src/main.rs
@@ -1,3 +1,49 @@
+// does not need lifetime annotations because returns an owned value not a ref
+fn double_lenght<T>(collection: &[T]) -> usize {
+    collection.len() * 2
+}
+
+// does not need because it inferred
+fn last_two<T>(vector: &[T]) -> &[T] {
+    if vector.len() >= 2 {
+        &vector[vector.len() - 2..]
+    } else {
+        &vector
+    }
+}
+
+// does need lifetime because it cannot be inferred due to two refs being used and one being returned
+fn first_five<'a>(text: &'a str, announcement: &str) -> &'a str {
+    println!("{}", announcement);
+    &text[..5]
+}
+
+// needs annotations due to three refs being used an one returned, lifetime cannot be infered
+fn find_string_that_has_content<'a>(first: &'a str, second: &'a str, target: &str) -> &'a str {
+    if first.contains(target) {
+        first
+    } else {
+        second
+    }
+}
+
+fn main() {
+    let numbers = vec![1, 2, 3, 4, 5];
+    println!("{}", double_lenght(&numbers));
+    println!("{:?}", last_two(&numbers));
+    let first = first_five("lucasguerra", "Henlo");
+    println!("{}", first);
+    println!(
+        "{}",
+        find_string_that_has_content("programming", "dining", "gram")
+    );
+    example();
+}
+
+//^ Define examples after main
+
+const COUNT: i32 = 400;
+
 // using the ' (tick) to define a lifetime annotation
 fn select_first_two<'a>(items: &'a [String]) -> &'a [String] {
     &items[..2]
@@ -11,7 +57,34 @@ fn longest<'a>(first: &'a str, second: &'a str) -> &'a str {
     }
 }
 
-fn main() {
+// struct cant outlive referene to avoid a dangling ref
+#[derive(Debug)]
+struct Dentist<'a> {
+    name: &'a str,
+}
+
+impl<'a> Dentist<'a> {
+    fn new(name: &'a str) -> Self {
+        Self { name }
+    }
+}
+
+#[derive(Debug)]
+struct TravelPlan<'a, 'b> {
+    from: &'a str,
+    to: &'b str,
+}
+
+// this works because &str is embedded in the binary
+fn static_fn() -> &'static str {
+    "hello"
+}
+
+fn give_count() -> &'static i32 {
+    &COUNT
+}
+
+fn example() {
     // non-lexical lifetime
     let mut data = vec![1, 2, 3];
     let slice = &mut data[..];
@@ -33,4 +106,25 @@ fn main() {
     let sf = "San Franciso";
     println!("{:?}", longest(&orlando, sf));
     println!("{}", orlando);
+
+    let dentist = Dentist::new("lucas");
+    println!("{:?}", dentist.name);
+
+    let from = String::from("BR");
+
+    let plan = {
+        let to = String::from("LK");
+        let travel = TravelPlan {
+            from: &from,
+            to: &to,
+        };
+
+        println!("{}", travel.to);
+
+        travel.from
+    };
+
+    println!("{}", plan);
+    println!("{}", give_count());
+    println!("{}", static_fn());
 }
