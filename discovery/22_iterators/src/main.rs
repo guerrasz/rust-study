3696454c18e0d3c50d5d77cc3cb00f8148d44ec7
guerrasz <lucasgasardo@gmail.com>
2025-12-19 18:10:35 -0300

    feat: update main function to demonstrate vector iteration and HashMap usage

--- discovery/22_iterators/src/main.rs
+++ discovery/22_iterators/src/main.rs
@@ -1,3 +1,23 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let my_list = vec![1, 2, 3, 4];
+    //reutrns iterator
+    //let mut iterator = my_list.into_iter();
+    //println!("{:?}", iterator);
+
+    for number in &my_list {
+        println!("{:?}", number);
+    }
+
+    println!("{:?}", my_list);
+
+    let mut todos = HashMap::new();
+    todos.insert("Groceries", false);
+    todos.insert("Study", true);
+    todos.insert("Sleep", false);
+
+    for (todo, completion) in &todos {
+        println!("Task: {}, status: {}", todo, completion)
+    }
 }
