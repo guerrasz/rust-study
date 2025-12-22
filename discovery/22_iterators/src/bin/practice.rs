c6c13a68b1a052874e9fe6710c80f2556e5027da
guerrasz <lucasgasardo@gmail.com>
2025-12-22 17:53:22 -0300

    feat: add examples for iterators with squares, name lengths, and evens filtering

--- discovery/22_iterators/src/bin/practice.rs
+++ discovery/22_iterators/src/bin/practice.rs
@@ -51,4 +51,30 @@ fn main() {
         "{:?}",
         count_chars("Lucas is giving his best learning rust")
     );
+
+    let numbers = vec![1, 2, 4, 8, 9, 13, 21];
+    // .iter() turns numbers into iterator
+    // .map() makes the actions inside the closure for each item
+    // .collect() collects in the resulting iterator again
+    let squares: Vec<u32> = numbers.iter().map(|number: &u32| number.pow(2)).collect();
+
+    println!("{:?}", squares);
+
+    let names = ["Lucas", "Rafel", "Allan", "Felipe"];
+
+    let name_lenghts: Vec<usize> = names
+        .iter()
+        .map(|name| name.to_lowercase())
+        .map(|name| name.replace("i", "@@"))
+        .map(|name| name.len())
+        .collect();
+
+    println!("{:?}", name_lenghts);
+
+    let evens: Vec<u32> = numbers
+        .into_iter()
+        .filter(|number| number % 2 == 0)
+        .collect();
+
+    println!("{:?}", evens);
 }
