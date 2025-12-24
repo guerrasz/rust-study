501af79c53305c98ec526f3b92130f67be6aa606
LucasGuerraS <lucasgasardo@gmail.com>
2025-12-24 12:24:02 -0300

    feat: update read_files to accept path as CLI argument

--- discovery/22_iterators/src/bin/read_files.rs
+++ discovery/22_iterators/src/bin/read_files.rs
@@ -1,12 +1,26 @@
 use std::fs;
 
-fn main() -> std::io::Result<()> {
-    for dir_entry in fs::read_dir("./")? {
+fn main() {
+    let path = std::env::args().nth(1).unwrap_or_else(|| {
+        eprintln!("Usage: read_files <path>",);
+        eprintln!("Error: No path specified as CLI argument");
+        std::process::exit(1);
+    });
+
+    match read_path_contents(&path) {
+        Ok(_) => std::process::exit(0),
+        Err(error) => eprintln!("Error reading file, {error}"),
+    }
+}
+
+fn read_path_contents(path: &str) -> std::io::Result<()> {
+    for dir_entry in fs::read_dir(path)? {
         let entry = dir_entry?;
         if entry.file_type()?.is_file() {
             println!("Filename: {:?}\n --------------", entry.file_name());
             println!("{}", fs::read_to_string(entry.path())?);
         }
     }
+
     Ok(())
 }
